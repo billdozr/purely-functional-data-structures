0072bb4029a1bdc4184035fcca8f5a0bad1ed311
Alen Ribic <alen.ribic@gmail.com>
2012-07-18 17:14:13 +0200

    Added first stab at 2.3

--- Chapter2.hs
+++ Chapter2.hs
@@ -19,6 +19,8 @@ suffixes xs = xs : (suffixes $ tail xs)
 
 data BSTree a = Empty | Node (BSTree a) a (BSTree a) 
                        deriving (Show, Eq)
+                                
+type AlreadyExists = String
 
 mkTree = 
   (Node 
@@ -34,6 +36,13 @@ member x (Node l v r)
   | x > v = member x r
   | otherwise = True
                 
+insert :: Ord a => a -> BSTree a -> BSTree a
+insert x Empty = Node Empty x Empty
+insert x t@(Node l v r)
+  | x < v = Node (insert x l) v r
+  | v < x = Node l v (insert x r)
+  | otherwise = t
+
 -- // --------------------------
 -- // Exercise 2.2
 -- // --------------------------
@@ -45,4 +54,19 @@ member2 x t@(Node l v r) = member' t v
           if x < y then
             member' a c
           else member' b y
+-- \\ --------------------------
+
+-- // --------------------------
+-- // Exercise 2.3
+-- // --------------------------
+insert2 :: Ord a => a -> BSTree a -> Either AlreadyExists (BSTree a)
+insert2 x t = 
+  case insert' t of 
+    Empty -> Left "Element already exists"
+    r -> Right r
+  where insert' Empty = Node Empty x Empty
+        insert' (Node l v r)
+          | x < v = Node (insert' l) v r
+          | v < x = Node l v (insert' r)
+          | otherwise = Empty
 -- \\ --------------------------
